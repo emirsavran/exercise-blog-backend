6a775a2dad3785f6df9ef5eff4e4039861adc4a3
Emir Savranogullari <emir@emirsavran.com>
2020-01-31 18:25:22 +0300

    create route for getting single post by id

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ app.use(cors());
 
 app.get('/posts', (req, res) => res.json(posts));
 
+app.get('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  // do not handle not found case for simplicity's sake
+  res.json(posts.find((post) => post.id === id));
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
