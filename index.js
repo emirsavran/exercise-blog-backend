30bbe0ee4824a747847ad0eaf941c8e27b7066c7
Emir Savranogullari <emir@emirsavran.com>
2020-01-31 15:50:43 +0300

    enable cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const posts = require('./posts');
 
 const app = express();
 const port = 3001;
+
+app.use(cors());
 
 app.get('/posts', (req, res) => res.json(posts));
 
